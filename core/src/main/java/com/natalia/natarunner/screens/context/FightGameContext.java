6f7387b0ba896068beaa1ab0215059e8bd9ab5c6
ngarre <a28710@svalero.com>
2026-04-18 13:33:33 +0200

    feat: preparo contexto nivel 2 y completo el del nivel 1

--- core/src/main/java/com/natalia/natarunner/screens/context/FightGameContext.java
+++ core/src/main/java/com/natalia/natarunner/screens/context/FightGameContext.java
@@ -1,16 +1,19 @@
 package com.natalia.natarunner.screens.context;
 
 import com.natalia.natarunner.config.GameConfig;
-import com.natalia.natarunner.model.entities.drops.RedDrop;
-import com.natalia.natarunner.model.entities.drops.WhiteDrop;
-import com.natalia.natarunner.model.entities.drops.YellowDrop;
-import com.natalia.natarunner.model.entities.player.PlayerTears;
-import com.natalia.natarunner.ui.*;
+import com.natalia.natarunner.model.entities.player.PlayerBladecar;
+import com.natalia.natarunner.model.entities.projectile.BulletChicas;
+import com.natalia.natarunner.ui.CountdownTimer;
+import com.natalia.natarunner.ui.FlashMessage;
+import com.natalia.natarunner.ui.FloatingText;
+import com.natalia.natarunner.ui.PauseMenu;
+import com.natalia.natarunner.ui.ScoreBar;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Array;
 
-public class TearsGameContext {
+public class FightGameContext {
 
     /*
      * Clase de contexto que centraliza el estado de la partida (player, score, entidades, timers, etc.).
@@ -19,54 +22,53 @@ public class TearsGameContext {
      * Forma parte de la separación de responsabilidades aplicada a la arquitectura de la Screen.
      */
 
-    public String level1Title = "LEVEL 1: TEARS DISTRICT";
+    public String level2Title = "LEVEL 2: FIGHT ZONE";
 
     public enum GameState {
         INTRO,
         PLAYING,
         PAUSED,
-        GAMEOVER
+        GAMEOVER,
+        YOUWIN
     }
-
 
     public GameState state = GameState.INTRO;
 
     public boolean freezeMode = false;
     public boolean pauseOnlyMusic = false;
-    public boolean dragging = false;
     public boolean initialized = false;
+    public boolean dragging = false;
 
-    public float hudHeight = 1f;
-    public float barHeight = 60f;
-
-    public float gotaBlancaTimer = 0f;
-    public float gotaAmarillaTimer = 0f;
-    public float gotaRojaTimer = 0f;
-    public float tiempoTotal = 0f;
+    public boolean bossPending = false;
 
     public int score = 100;
     public int scoreBasta;
 
     public int hearts = GameConfig.cuantosCorazones;
 
+    public int npcKilled = 0;
 
-    public PlayerTears playerTears;
+    public float hudHeight = 1f;
+    public float barHeight = 60f;
 
-    public Array<WhiteDrop> gotasBlancas = new Array<>();
-    public Array<YellowDrop> gotasAmarillas = new Array<>();
-    public Array<RedDrop> gotasRojas = new Array<>();
+    public float bossDelayTimer = 0f;
+    public float bossDelay = 1f;
+
+    public Rectangle level2Rectangulo = new Rectangle();
+
+    public Vector2 mouseHud = new Vector2();
+    public Vector2 mouseWorld = new Vector2();
+
+    public PlayerBladecar player;
+
+    public Array<BulletChicas> bullets = new Array<>();
     public Array<FloatingText> floatingTexts = new Array<>();
 
     public FlashMessage flashMessage;
+    public CountdownTimer countdownTimer;
     public PauseMenu pauseMenu;
     public ScoreBar scoreBar;
-    public CountdownTimer countdownTimer;
-
-    public Rectangle level1Rectangulo = new Rectangle();
 
     public BitmapFont font;
     public BitmapFont smallFont;
-
-    public com.badlogic.gdx.utils.Array<com.natalia.natarunner.model.entities.projectile.RedDropProjectile> redProjectiles =
-        new com.badlogic.gdx.utils.Array<>();
 }
